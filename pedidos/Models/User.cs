0eaf7631e17bc446b84784bd23a1952053628077
fabiCas-D-04 <fcd040206@gmail.com>
2025-09-16 20:36:45 -0400

    4to paso modificacion del Model(User.cs)

--- pedidos/Models/User.cs
+++ pedidos/Models/User.cs
@@ -2,5 +2,9 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
     }
 }
