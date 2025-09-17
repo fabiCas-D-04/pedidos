816e43aea41cca1e9fef63966254d241217c69e9
fabiCas-D-04 <fcd040206@gmail.com>
2025-09-16 20:37:42 -0400

    5to paso modificacion Model(Product.cs)

--- pedidos/Models/Product.cs
+++ pedidos/Models/Product.cs
@@ -2,5 +2,9 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Stock { get; set; }
     }
 }
