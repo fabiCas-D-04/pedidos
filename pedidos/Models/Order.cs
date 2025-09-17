808908e989ba7ee8e5b94840ec70d4a5fc2ec617
fabiCas-D-04 <fcd040206@gmail.com>
2025-09-16 20:41:17 -0400

    7mo paso modificacion Model(Order.cs)

--- pedidos/Models/Order.cs
+++ pedidos/Models/Order.cs
@@ -2,5 +2,9 @@
 {
     public class Order
     {
+        public int Id { get; set; }
+        public int Cliente { get; set; }
+        public int Estado { get; set; }
+        public DateTime Fecha { get; set; }
     }
 }
